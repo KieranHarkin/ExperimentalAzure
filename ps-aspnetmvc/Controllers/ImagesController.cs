ba3eae3e762ce5691421f0b7833222f395102d0e
Kieran Harkin <kieranharkin@googlemail.com>
2017-05-20 00:29:32 +0100

    Set up blob storage and upload image

---
+++ ps-aspnetmvc/Controllers/ImagesController.cs
@@ -0,0 +1,40 @@
+﻿using ps_aspnetmvc.Data;
+using ps_aspnetmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ps_aspnetmvc.Controllers
+{
+    public class ImagesController : Controller
+    {
+        ImageStore _store = new ImageStore();
+
+        // GET: Images
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> Upload(HttpPostedFileBase image)
+        {
+            if (image != null)
+            {
+                var imageId = await _store.SaveImage(image.InputStream);
+                return RedirectToAction(nameof(Show), new { id = imageId });
+            }
+
+            return View();
+        }
+
+        public ActionResult Show(string id)
+        {
+            var model = new ShowModel { Uri = _store.UriFor(id) };
+            return View(model);
+        }
+    }
+}
---
+++ ps-aspnetmvc/Data/ImageStore.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ps_aspnetmvc.Data
+{
+    public class ImageStore
+    {
+        Uri _baseUri = new Uri("https://hakopscoursestore.blob.core.windows.net/");
+        CloudBlobClient _client;
+
+        public ImageStore()
+        {
+            _client = new CloudBlobClient(_baseUri, new StorageCredentials("hakopscoursestore", "7SmKRHklsSzNyXs3IM78/O1njwOq93Dvqd8bj2HQPnJX6f4ikI45+Ll68wo17AYmCU02tE/DzH/Y6EyGXkOysg=="));
+        }
+
+        public async Task<string> SaveImage(Stream stream)
+        {
+            var id = Guid.NewGuid().ToString();
+            var container = _client.GetContainerReference("images");
+            var blob = container.GetBlockBlobReference(id);
+            await blob.UploadFromStreamAsync(stream);
+            return id;
+        }
+
+        public Uri UriFor(string imageId)
+        {
+            return new Uri(_baseUri, $"/images/{imageId}");
+        }
+    }
+}
